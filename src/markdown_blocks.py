3e69c6a5a0362928c6d4d26a0a58703538ea280b
srinivassivaratri <srinivassivaratri1122@gmail.com>
2024-05-19 22:50:11 +0530

    feat: Refactor markdown_blocks.py for improved readability and maintainability

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,7 @@
+from html_node import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
 block_type_code = "code"
@@ -17,6 +21,31 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_node(block)
+    if block_type == block_type_heading:
+        return heading_to_html_node(block)
+    if block_type == block_type_code:
+        return code_to_html_node(block)
+    if block_type == block_type_quote:
+        return quote_to_html_node(block)
+    if block_type == block_type_unordered_list:
+        return unordered_list_to_html_node(block)
+    if block_type == block_type_ordered_list:
+        return ordered_list_to_html_node(block)
+    raise ValueError("Invalid block type")
+
 
 def block_to_block_type(block):
     # Headings start with 1-6 # characters, followed by a space and then the heading text.
@@ -41,18 +70,81 @@ def block_to_block_type(block):
         return block_type_ordered_list
     else:
         return block_type_paragraph
-     
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = "".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == '#':
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalid heading level: {level}")
+    text = block[level + 1: ]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+def code_to_html_node(block):
+    if not block.startswith('```') or not block.endswith('```'):
+        raise ValueError("Invalid code block")
+    text = block[4 : -3]
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+def ordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3: ]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+def unordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2: ]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+
+
+
+
+
+
+
+
+   
     
-    
 
 
 
-# def markdown_to_blocks(markdown):
-#     # Its job is to split the input string into distinct blocks and strip any leading or trailing whitespace from each block. It should also remove any "empty" blocks due to excessive newlines.
-#     blocks = markdown.split("\n\n")
-#     for block in blocks: 
-#         block.strip()
-#         if block == "":
-#             blocks.remove(block)
-#     return blocks
-          
